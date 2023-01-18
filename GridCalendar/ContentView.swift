043a89a922dcb1f79eae122d9780122772acc1f2
smichaelonline <stephaniemichael001@gmail.com>
2023-01-18 13:57:12 -0500

    Update formatting and allow headers to be sticky as user scrolls

--- GridCalendar/ContentView.swift
+++ GridCalendar/ContentView.swift
@@ -9,19 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     let layout = [
-        GridItem(.fixed(40)),
-        GridItem(.fixed(40)),
-        GridItem(.fixed(40)),
-        GridItem(.fixed(40)),
+        GridItem(.flexible(minimum: 40)),
+        GridItem(.flexible(minimum: 40)),
+        GridItem(.flexible(minimum: 40)),
+        GridItem(.flexible(minimum: 40)),
+        GridItem(.flexible(minimum: 40)),
+        GridItem(.flexible(minimum: 40)),
+        GridItem(.flexible(minimum: 40))
     ]
     
     var body: some View {
         ScrollView {
-            LazyVGrid(columns: layout) {
-                ForEach(year[0].days) {
-                    day in Capsule().overlay(Text("\(day.value)").foregroundColor(.white))
-                        .foregroundColor(.blue)
-                        .frame(height: 40)
+            LazyVGrid(columns: layout, pinnedViews: [.sectionHeaders]) {
+                ForEach(year, id:\.name) {
+                    month in Section(header: Text(verbatim: month.name).font(.headline)) {
+                        ForEach(month.days) { day in Capsule()
+                                .overlay(Text("\(day.value)").foregroundColor(.white))
+                                        .foregroundColor(.blue)
+                                        .frame(height:40)
+                        }
+                    }
                 }
             }
         }
