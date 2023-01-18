ea710d3d945e551b14335c2e1aaab4b40cc6dce5
smichaelonline <stephaniemichael001@gmail.com>
2023-01-18 11:07:26 -0500

    Initial Commit

---
+++ GridCalendar/GridCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalendarApp.swift
+//  GridCalendar
+//
+//  Created by Stephanie Michael on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct GridCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
