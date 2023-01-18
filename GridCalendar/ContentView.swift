ae661b834cd80a213c43b4cac6a6552f63f927f2
smichaelonline <stephaniemichael001@gmail.com>
2023-01-18 12:20:29 -0500

    Add Day and Month structures

--- GridCalendar/ContentView.swift
+++ GridCalendar/ContentView.swift
@@ -19,6 +19,27 @@ struct ContentView: View {
     }
 }
 
+struct Day: Identifiable {
+    let id = UUID()
+    let value: Int
+}
+
+struct Month {
+    let name: String
+    let numberOfDays: Int
+    var days: [Day]
+    
+    init(name: String, numberOfDays: Int) {
+        self.name = name
+        self.numberOfDays = numberOfDays
+        self.days = []
+        
+        for n in 1...numberOfDays {
+            self.days.append(Day(value: n))
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
